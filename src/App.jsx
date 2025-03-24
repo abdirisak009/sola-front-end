4ddb579956e1d1108ac0b97b82a5fe156008ba44
Maxamed459 <maxamedmahdi459@gmail.com>
2025-03-25 01:54:36 +0300

    add changes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import React from "react";
-import LifestyleMindset from "./pages/LifestyleMindset";
-import Header from "./components/Header";
 
 
 const App = () => {
   return (
     <div>
-      <LifestyleMindset />
-      {/* <h1 className="text-blue-600 text-7xl text-center font-bold">
+      <h1 className="text-blue-600 text-7xl text-center font-bold">
         Hello My app
-      </h1> */}
+      </h1>
     </div>
   );
 };
