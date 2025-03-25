65db38322dc7207e86a718b73fd59056af12ff5b
Maxamed459 <maxamedmahdi459@gmail.com>
2025-03-25 04:04:44 +0300

    modified something.

--- src/components/PowerofMindset.jsx
+++ src/components/PowerofMindset.jsx
@@ -12,7 +12,7 @@ const PowerofMindset = () => {
                 <h1 className='text-3xl md:text-4xl font-bold tracking-wide text-[#000] text-center max-w-3xl mx-auto'>The Power of Mindset </h1>
                 <p className='text-[16px] text-gray-500 text-center max-w-[85%] mx-auto'>Your thoughts shape your reality. Cultivate a positive mindset to transform your health journey</p>
             </div>
-            <div className='grid grid-cols-3 gap-4 mt-10'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10'>
                 <PowermindsetCard icon={LuBrain} title="Growth Mindset" text="Embrace challenges as opportunities for growth. View setbacks as temporary and valuable learning experiences rather than permanent failures"/>
 
                 <PowermindsetCard icon={FaRegFaceSmile} title="Positive Self-Talk" text="The way you speak to yourself matters. Replace self-criticism with compassionate, encouraging internal dialogue to build confidence and resilience."/>
