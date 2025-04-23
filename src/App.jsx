d109a3ef3b0e5231f430dacb4a5fa9b5096ea49a
omar <omar@gmail.com>
2025-04-23 09:20:06 +0300

    adssf

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
-import React from 'react'
+import React from "react";
+import Nutrition from "./pages/Nutrition";
 
 const App = () => {
   return (
-    <></>
-  )
-}
+    <>
+      <Nutrition />
+    </>
+  );
+};
 
-export default App
-
+export default App;
